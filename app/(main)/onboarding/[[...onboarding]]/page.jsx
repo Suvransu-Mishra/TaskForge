53afe0765332cf976bdcb9a27632a79408153235
Suvransu <suvmishraa@gmail.com>
2025-07-21 01:28:58 +0530

    Updaeted code

--- app/(main)/onboarding/[[...onboarding]]/page.jsx
+++ app/(main)/onboarding/[[...onboarding]]/page.jsx
@@ -1,26 +1,14 @@
 "use client";
 
-import { OrganizationList, useOrganization } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { OrganizationList } from "@clerk/nextjs";
 
 export default function Onboarding() {
-  const { organization } = useOrganization();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (organization) {
-      router.push(`/organization/${organization.slug}`);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [organization]);
-
   return (
     <div className="flex justify-center items-center pt-14">
       <OrganizationList
         hidePersonal
-        afterCreateOrganizationUrl="/organization/:slug"
-        afterSelectOrganizationUrl="/organization/:slug"
+        afterCreateOrganizationUrl="/organization"
+        afterSelectOrganizationUrl="/organization"
       />
     </div>
   );
